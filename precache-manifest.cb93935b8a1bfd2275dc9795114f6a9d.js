d43bd909879b41ac173b0db94ecf493f3abf9e47
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-10 02:24:19 +0000

    Deploy huhn511/im to github.com/huhn511/im.git:gh-pages

---
+++ precache-manifest.cb93935b8a1bfd2275dc9795114f6a9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce52eb51139bb451fbf9",
+    "url": "/im/css/app.1a6fd85f.css"
+  },
+  {
+    "revision": "ce52eb51139bb451fbf9",
+    "url": "/im/js/app.4c9474d6.js"
+  },
+  {
+    "revision": "7896f75ac89ab18f65ca",
+    "url": "/im/js/chunk-vendors.026b025a.js"
+  },
+  {
+    "revision": "f444a2870475b59adfd8437cec4df177",
+    "url": "/im/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/im/robots.txt"
+  }
+];
